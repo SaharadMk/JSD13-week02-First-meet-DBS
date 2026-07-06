3e5409c851a1acdf4550d58db5ab30df41bea26e
Saharad Chimdonthong <saharad.ch@gmail.com>
2026-07-06 16:26:52 +0700

    add DB-EX2

---
+++ queryEX2.mongodb.js
@@ -0,0 +1,7 @@
+use("sample_mflix");
+
+//find example of type movie and rated "TV-G"
+//db.movies.findOne({ type: "movie", rated: "TV-G" });
+
+//find how many movie are there type movie and rated "TV-G"
+db.movies.find({ type: "movie", rated: "TV-G" }).count();
